3e4e11a9ecc3cbb71bdc16a1b1280ea8ce006bad
haiseong <wjdgotjd9908@gmail.com>
2022-11-11 02:19:46 +0900

    Feat/#5 - 메이크파일과 테스트 파일 헤더파일 수정

--- Source/Module/UI/UI.h
+++ Source/Module/UI/UI.h
@@ -4,10 +4,23 @@
 # include <stdio.h>
 # include <Windows.h>
 
+
+#define CARD_POS_X_NPC_L 70
+#define CARD_POS_Y_NPC_L 8
+#define CARD_POS_X_NPC_R 150
+#define CARD_POS_Y_NPC_R 8
+#define CARD_POS_X_PERSON_L 70
+#define CARD_POS_Y_PERSON_L 39
+#define CARD_POS_X_PERSON_R 150
+#define CARD_POS_Y_PERSON_R 39
+
 // UIUtil
 void setCurrentCursorPos(int x, int y);
 COORD getCurrentCursorPos(void);
 
 void initTerminal(void);
+void drawScreen(void);
+void drawCard(int cardID, int posX, int posY);
+void deleteCard(int posX, int posY);
 
 #endif
--- Source/Module/UI/test.c
+++ Source/Module/UI/test.c
@@ -5,5 +5,23 @@
 int main()
 {
 	initTerminal();
+
+    drawScreen();
+
+	for (int i = 0; i < 100; i++)
+	{
+		
+		deleteCard(CARD_POS_X_NPC_L, CARD_POS_Y_NPC_L);
+		drawCard(rand() % 20, CARD_POS_X_NPC_L, CARD_POS_Y_NPC_L);
+		deleteCard(CARD_POS_X_NPC_R, CARD_POS_Y_NPC_R);
+		drawCard(rand() % 20, CARD_POS_X_NPC_R, CARD_POS_Y_NPC_R);
+		deleteCard(CARD_POS_X_PERSON_L, CARD_POS_Y_PERSON_L);
+		drawCard(rand() % 20, CARD_POS_X_PERSON_L, CARD_POS_Y_PERSON_L);
+		deleteCard(CARD_POS_X_PERSON_R, CARD_POS_Y_PERSON_R);
+		drawCard(rand() % 20, CARD_POS_X_PERSON_R, CARD_POS_Y_PERSON_R);
+		Sleep(2000);
+
+	}
+
 	getchar();
 }
