80635453e0b944ce4e3743348b6038365ed3ea49
mooyoung2309 <mooyoung2309@gmail.com>
2022-11-15 19:02:44 +0900

    [Refactor] #24 - 파일 쪼개기

--- Source/Module/Item/item.h
+++ Source/Module/Item/item.h
@@ -1,15 +1,6 @@
 #ifndef item_H
 # define item_H
 
-typedef enum ItemId { RuleChange = 0, Star = 1, Bomb = 2, Win = 3 } ItemId;
-typedef enum ItemState { Active = 0, Inactive = 1, Deleted = 2 } ItemState;
-
-typedef struct Item {
-	ItemId id;
-	ItemState state;
-	int roundCount;
-} Item;
-
 Item* getNextItem();
 void checkAndRunItem(Item *item);
 void putDownItem(Item *item);
