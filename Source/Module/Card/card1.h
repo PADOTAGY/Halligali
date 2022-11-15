714f45d894fd42eef81078fc7fa2533ba847be77
jujoko <jujoko123456@gmail.com>
2022-11-15 22:13:52 +0900

    [Feat] #26 - 카드 만들기 코드 리뷰 반영

--- Source/Module/Card/card1.h
+++ Source/Module/Card/card1.h
@@ -1,11 +1,5 @@
-#ifndef UI_H
-#define UI_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <Windows.h>
-#include <time.h>
-#include <conio.h>
+#ifndef CARD_H
+#define CARD_H
 
 #define LEFT 75
 #define RIGHT 77
--- Source/Module/Reusable/Reusable.h
+++ Source/Module/Reusable/Reusable.h
@@ -1,9 +1,12 @@
 #ifndef reusable_H
-# define reusable_H
+#define reusable_H
 
 #include "../module.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
+#include <time.h>
+#include <conio.h>
 
 #endif
