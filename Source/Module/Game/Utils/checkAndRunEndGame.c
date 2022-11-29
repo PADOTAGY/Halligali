264f42668f6eca3b00809ec11bb92fe9c82ef5e1
haiseong <wjdgotjd9908@gmail.com>
2022-11-29 23:55:25 +0900

    [feat/#56] - Game/Util 오류 수정

--- Source/Module/Game/Utils/checkAndRunEndGame.c
+++ Source/Module/Game/Utils/checkAndRunEndGame.c
@@ -14,7 +14,7 @@ int checkAndRunEndGame(Game *game) {
 }
 
 void updateGameState(Game *game) {      // 카드수 둘다 0이거나  
-    if (((game->whoBell!=-1) && (npc->originCardSet->count == 0 || game->user->originCardSet->count == 0)) || (npc->originCardSet->count == 0 && game->user->originCardSet->count == 0))
+    if (((game->whoBell!=-1) && (game->npc->originCardSet->count == 0 || game->user->originCardSet->count == 0)) || (game->npc->originCardSet->count == 0 && game->user->originCardSet->count == 0))
         game->state = End;
 }
 
